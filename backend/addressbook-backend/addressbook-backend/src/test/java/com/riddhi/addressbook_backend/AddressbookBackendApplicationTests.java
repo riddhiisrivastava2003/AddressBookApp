656f1a379fe13286c5cf994fe04466f62634ee64
Riddhi Srivastava <riddhisrivastava2003@gmail.com>
2026-03-09 12:24:13 +0530

    [Riddhi Srivastava] Add .added initial setup

---
+++ backend/addressbook-backend/addressbook-backend/src/test/java/com/riddhi/addressbook_backend/AddressbookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riddhi.addressbook_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressbookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
